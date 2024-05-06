f8757dc508ab70fa90f382faac2b273024f0b959
Alex Efimenkoff <etigiloff@gmail.com>
2024-05-07 01:07:33 +0300

    add click event for innul generate button

---
+++ scripts/script.js
@@ -0,0 +1,64 @@
+function generateINNUL(part){
+    return "pass"
+}
+
+function generateKPP(part){
+    return "pass"
+}
+
+function generateOGRN(part){
+    return "pass"
+}
+
+function generateINNFL(part){
+    return "pass"
+}
+
+function generateOGRNIP(part){
+    return "pass"
+}
+
+function generateSNILS(part){
+    return "pass"
+}
+
+function generateUUID(part){
+    return "pass"
+}
+
+function generateFIO(part){
+    return "pass"
+}
+
+function generateOrganizationName(part){
+    return "pass"
+}
+
+function generateBIN(part){
+    return "pass"
+}
+
+function generateIIN(part){
+    return "pass"
+}
+
+function generateUNN(part){
+    return "pass"
+}
+
+function generateUNP(part){
+    return "pass"
+}
+
+function generateAll(part){
+    return "pass"
+}
+
+function deleteAll(part){
+    return "pass"
+}
+
+const generateINNULBtn = document.getElementById("generateINNULBtn");
+generateINNULBtn.addEventListener("click", function(){
+    document.getElementById("generateINNULInput").value = generateINNUL("");
+})
