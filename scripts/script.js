8a0cdcf22c398829d18cb492e7966966f5679a32
Alex Efimenkoff <etigiloff@gmail.com>
2024-05-08 01:50:14 +0300

    add generate inn ul

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,31 @@
 function generateINNUL(part){
-    return "pass"
+    let inn = []
+    for(let i = 0; i < 10; i++){
+        inn[i] = Math.floor(Math.random() * 10);
+    }
+    const cof = [2, 4, 10, 3, 5, 9, 4, 6, 8];
+    let sum = 0;
+    for(let i = 0; i < cof.length; i++){
+        sum += inn[i] * cof[i];
+    }
+    inn[9] = sum % 11;
+    let result = '';
+    for(let i = 0; i < inn.length; i++){
+        result += inn[i].toString();
+    }
+    return result;
+}
+function validateINNUL(value){
+    if (value.length != 10) return false;
+    if (isNaN(parseInt(value))) return false;
+    let valueToArray = value.split('');
+    const cof = [2, 4, 10, 3, 5, 9, 4, 6, 8];
+    let sum = 0;
+    for(let i = 0; i < cof.length; i++){
+        sum += valueToArray[i] * cof[i];
+    }
+    if (valueToArray[9] != sum % 11) return false;
+    return true;
 }
 
 function generateKPP(part){
@@ -59,6 +85,27 @@ function deleteAll(part){
 }
 
 const generateINNULBtn = document.getElementById("generateINNULBtn");
+const copyINNULBtn = document.getElementById("copyINNULBtn");
+const INNULInput = document.getElementById("INNULInput");
+
 generateINNULBtn.addEventListener("click", function(){
-    document.getElementById("generateINNULInput").value = generateINNUL("");
-})
+    INNULInput.value = generateINNUL("");
+});
+
+copyINNULBtn.addEventListener("mouseover", function() {
+    if (validateINNUL(INNULInput.value)) {
+        copyINNULBtn.classList.add('validation-passed');
+        copyINNULBtn.classList.remove('validation-failed');
+        copyINNULBtn.classList.remove('validation-off');
+    } 
+    else {
+        copyINNULBtn.classList.add('validation-failed');
+        copyINNULBtn.classList.remove('validation-passed');
+        copyINNULBtn.classList.remove('validation-off');
+    }
+});
+copyINNULBtn.addEventListener("mouseout", function() {
+        copyINNULBtn.classList.add('validation-off');
+        copyINNULBtn.classList.remove('validation-failed');
+        copyINNULBtn.classList.remove('validation-passed');
+});
