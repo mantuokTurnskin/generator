0aa6d82e7c18842694e72af1487c63f1e6fe2e2d
Alex Efimenkoff <etigiloff@gmail.com>
2024-05-11 20:26:07 +0300

    fix INNUL

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,60 @@
-function generateINNUL(part){
-    let inn = []
-    for(let i = 0; i < 10; i++){
-        inn[i] = Math.floor(Math.random() * 10);
-    }
-    const cof = [2, 4, 10, 3, 5, 9, 4, 6, 8];
-    let sum = 0;
-    for(let i = 0; i < cof.length; i++){
-        sum += inn[i] * cof[i];
-    }
-    inn[9] = sum % 11;
-    let result = '';
-    for(let i = 0; i < inn.length; i++){
-        result += inn[i].toString();
-    }
-    return result;
-}
-function validateINNUL(value){
-    if (value.length != 10) return false;
-    if (isNaN(parseInt(value))) return false;
-    let valueToArray = value.split('');
-    const cof = [2, 4, 10, 3, 5, 9, 4, 6, 8];
-    let sum = 0;
-    for(let i = 0; i < cof.length; i++){
-        sum += valueToArray[i] * cof[i];
-    }
-    if (valueToArray[9] != sum % 11) return false;
-    return true;
-}
+const INNUL = {
+    generateBtn: document.getElementById("generateINNULBtn"),
+    copyBtn: document.getElementById("copyINNULBtn"),
+    input: document.getElementById("INNULInput"),
+    validationElement: document.getElementsByClassName("INNULValidation")[0],
+    getNewValue: function(part) {
+        let inn = []
+        /*
+            Функция на основании веденного значения в поле ИННЮЛ генерирует через
+            random значение ИНН по алгоритму
+        */
+        if (isNaN(parseInt(part)) || part.length >= 10) part='';
+        if (part.length < 10) inn = part.split('');
+        for(let i = part.length; i < 10; i++) inn[i] = Math.floor(Math.random() * 10);
+        const cof = [2, 4, 10, 3, 5, 9, 4, 6, 8];
+        let sum = 0;
+        for(let i = 0; i < cof.length; i++) sum += inn[i] * cof[i];
+        inn[9] = (sum % 11 < 10)? sum % 11 : 0;
+        return inn.join('');
+    },
+    setNewValue: function() {
+        /*
+            Метод устанавливает сгенерированное значение в поле ИННЮЛ
+        */
+        this.input.value = this.getNewValue(this.input.value);  
+    },
+    validateValue: function(value){
+        if (value.length != 10) return false;
+        if (isNaN(parseInt(value))) return false;
+        let valueToArray = value.split('');
+        const cof = [2, 4, 10, 3, 5, 9, 4, 6, 8];
+        let sum = 0;
+        for(let i = 0; i < cof.length; i++){
+            sum += valueToArray[i] * cof[i];
+        }
+        if (valueToArray[9] != sum % 11) return false;
+        return true;
+    },
+    setValidation: function() {
+        if(this.validateValue(this.input.value)){
+            this.validationElement.classList.add('validation-passed');
+            this.validationElement.classList.remove('validation-failed');
+            this.validationElement.classList.remove('validation-off');
+        }
+        else{
+            this.validationElement.classList.add('validation-failed');
+            this.validationElement.classList.remove('validation-passed');
+            this.validationElement.classList.remove('validation-off');
+        }
+    },
+    copyValue: function() {
+        if (INNUL.input.value) {
+        navigator.clipboard.writeText(INNUL.input.value);
+        console.log(`Значение ИННЮЛ: ${INNUL.input.value} скопироано в буфер обмена.`)
+        }
+    }   
+};
 
 function generateKPP(part){
     return "pass"
@@ -84,28 +112,10 @@ function deleteAll(part){
     return "pass"
 }
 
-const generateINNULBtn = document.getElementById("generateINNULBtn");
-const copyINNULBtn = document.getElementById("copyINNULBtn");
-const INNULInput = document.getElementById("INNULInput");
-
-generateINNULBtn.addEventListener("click", function(){
-    INNULInput.value = generateINNUL("");
+// Слушаем события блока ИННЮЛ
+INNUL.generateBtn.addEventListener("click", ()=> {
+    INNUL.setNewValue();
+    INNUL.setValidation();
 });
-
-copyINNULBtn.addEventListener("mouseover", function() {
-    if (validateINNUL(INNULInput.value)) {
-        copyINNULBtn.classList.add('validation-passed');
-        copyINNULBtn.classList.remove('validation-failed');
-        copyINNULBtn.classList.remove('validation-off');
-    } 
-    else {
-        copyINNULBtn.classList.add('validation-failed');
-        copyINNULBtn.classList.remove('validation-passed');
-        copyINNULBtn.classList.remove('validation-off');
-    }
-});
-copyINNULBtn.addEventListener("mouseout", function() {
-        copyINNULBtn.classList.add('validation-off');
-        copyINNULBtn.classList.remove('validation-failed');
-        copyINNULBtn.classList.remove('validation-passed');
-});
+INNUL.input.addEventListener('mouseout', ()=> INNUL.setValidation());
+INNUL.copyBtn.addEventListener('click', ()=> INNUL.copyValue());
